0e6ed1e6120fe8e0c098fc698b138f5efec7f269
oneRaSol <67533138+oneRaSol@users.noreply.github.com>
2023-02-01 18:33:05 -0500

    Basic clean up

--- formSubmit.js
+++ formSubmit.js
@@ -6,28 +6,28 @@ class BlogData{
         this.content = content;
     }
 }
-All_Data = new Array();    
+All_Data = new Array();
 function submitData(){
 
     alert("Thank you for submitting a blog. We will review it and upload it")
-    
+
     let fname = document.forms["myForm"]["fname"].value;
     let lname = document.forms["myForm"]["lname"].value;
     let title = document.forms["myForm"]["title"].value;
     let content = document.forms["myForm"]["content"].value;
-    
-    
+
+
 
     const obj = new BlogData(fname,lname,title,content);
-   
+
     All_Data.push(obj);
     console.log(All_Data);
-   
+
    document.forms["myForm"]["fname"].value='';
-  document.forms["myForm"]["lname"].value='';
-     document.forms["myForm"]["title"].value='';
-    document.forms["myForm"]["content"].value='';
-    
+   document.forms["myForm"]["lname"].value='';
+   document.forms["myForm"]["title"].value='';
+   document.forms["myForm"]["content"].value='';
+
     return false;
 
 }
